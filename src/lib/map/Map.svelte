c605544f624111b09de63678ffa38a8d19614a40
azgian <azgian@gmail.com>
2024-09-20 10:13:38 +0900

    VITE_GOOGLE_MAPS_API_KEY

--- src/lib/map/Map.svelte
+++ src/lib/map/Map.svelte
@@ -8,7 +8,7 @@
 
   let mapElement: HTMLElement;
   let map: google.maps.Map;
-    const apiKey = import.meta.env.GOOGLE_MAPS_API_KEY;
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   
   onMount(() => {
     if (!window.google) {
